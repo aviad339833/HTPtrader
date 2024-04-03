01e71735724e646454c44ad47c080250310115bc
«KaterynaLevchuk» <katerinalev4uk@gmail.com>
2024-04-03 18:08:14 +0300

    setted up the piteas swap using the sdk api

--- scripts/trade_2.ts
+++ scripts/trade_2.ts
@@ -1,50 +1,66 @@
 // Necessary imports.
-import { ethers } from "hardhat";
 import router_ABI from "../abis/piteas_router_ABI.json";
-import { toBeHex } from "ethers";
+import axios from "axios";
+import { BigNumber, ethers } from "ethers";
+
 require("dotenv").config();
 
+const getQuote = async (
+  tokenInAddress: string,
+  tokenOutAddress: string,
+  amount: BigNumber,
+  allowedSlippage: number
+) => {
+  try {
+    const quoteUrl = `https://sdk.piteas.io/quote?tokenInAddress=${tokenInAddress}&tokenOutAddress=${tokenOutAddress}&amount=${amount}&allowedSlippage=${allowedSlippage}`;
+    const response = await axios.get(quoteUrl);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to get quote");
+  }
+};
+
 async function main() {
-  const tradeAmountPLS = "100"; // Define the amount of PLS you want to swap.
   const walletPrivateKey = process.env.WALLET_PK_2!;
   const RPC_URL = process.env.LIVE_RPC;
-  const HTP_ADDRESS = "0x7c7ba94b60270bc2c7d98d3498b5ce85b870a749";
-  const WRAP_CONTRACT_ADDRESS = "0xA1077a294dDE1B09bB078844df40758a5D0f9a27"; // Address of WPLS token contract.
+  const IN_ADDRESS = "PLS"; // Address of PLS token contract.
+  // const IN_ADDRESS = "0xA1077a294dDE1B09bB078844df40758a5D0f9a27"; // Address of WPLS token contract.
+  const OUT_ADDRESS = "0x7c7ba94b60270bc2c7d98d3498b5ce85b870a749"; // Address of HTP token contract.
   const ROUTER_ADDRESS = process.env.ROUTER_ADDRESS!; // Ensure you have this in your .env for the router's contract address
 
-  const provider = new ethers.JsonRpcProvider(RPC_URL);
+  const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
   const signer = new ethers.Wallet(walletPrivateKey, provider);
 
-  // Instantiate contracts with the provided ABI and signer.
-  const routerContract = new ethers.Contract(
-    ROUTER_ADDRESS,
-    router_ABI,
-    signer
-  );
+  // collect required data for tx
+  const nonce = await provider.getTransactionCount(signer.address, "pending");
+  const gasPrice = await provider.getGasPrice();
+  const gasLimit = 3000000;
+  const tradeAmountPLS = 100;
+  const amountIn = ethers.utils.parseUnits(tradeAmountPLS.toString(), "ether"); // Convert the amount to the correct unit.
+  const quote = await getQuote(IN_ADDRESS, OUT_ADDRESS, amountIn, 0.5);
+  const chainId = (await provider.getNetwork()).chainId;
 
-  const amountIn = ethers.parseUnits(tradeAmountPLS, "ether"); // Convert the amount to the correct unit.
-
-  // Prepare the swap details based on the ABI's requirements
-  const swapDetails = {
-    srcToken: WRAP_CONTRACT_ADDRESS,
-    destToken: HTP_ADDRESS,
-    destAccount: signer.address, // Assuming you want the swapped tokens to be sent to the same address
-    srcAmount: amountIn,
-    destMinAmount: 0, // Set to 0 or a minimum acceptable amount of the dest token
+  // prepate tx structure
+  const tx = {
+    nonce: nonce,
+    gasPrice: gasPrice,
+    gasLimit: gasLimit,
+    to: ROUTER_ADDRESS, // Piteas Router Address
+    data: quote.methodParameters.calldata,
+    value: amountIn,
+    chainId,
   };
 
-  // No extra data needed for this swap, hence passing an empty bytes array
-  const data = "0x";
+  console.log("tx: ", tx);
 
-  // Execute the swap
-  const txResponse = await routerContract.swap(swapDetails, data, {
-    value: amountIn, // Necessary if the source token is ETH or WETH, otherwise adjust accordingly
-    gasLimit: toBeHex(250000), // Estimate or adjust the gas limit as necessary
-  });
+  // sign and send the tx
+  const signedTx = await signer.signTransaction(tx);
+  const txResponse = await provider.sendTransaction(signedTx);
+  console.log("Transaction sent:", txResponse.hash);
+  const receipt = await txResponse.wait();
 
-  console.log(`Transaction hash: ${txResponse.hash}`);
-  await txResponse.wait();
-  console.log("Transaction confirmed!");
+  console.log("Transaction mined:", receipt);
 }
 
 main().catch((error) => {
